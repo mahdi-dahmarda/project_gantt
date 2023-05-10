dd5ee9ec5247e3e7748974c12a1c05852528aae6
mahdi-dahmarda <mahdi.dahmardah@gmail.com>
2023-05-10 15:46:59 +0430

    add simple config info in manifest file

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Project Gantt View",
+    "depends": ['project'],
+    "data": [
+        # "security/ir.model.access.csv",
+        # "views/estate_property_offer_views.xml",
+        # "data/master_data.xml",
+    ],
+    "demo": [],
+    "application": True,
+    "license": "GPL-3",
+}
