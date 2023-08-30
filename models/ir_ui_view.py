e350aa0aac2ebf6fc888a9254030dbd6b067a088
halizada2018 <128473122+halizada2018@users.noreply.github.com>
2023-08-30 08:40:38 +0430

    Icon added
    an icon added to the project_gantt module

--- models/ir_ui_view.py
+++ models/ir_ui_view.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 
 class View(models.Model):
     _inherit = "ir.ui.view"
+    _description = "Gantt Chart"
 
     type = fields.Selection(
         selection_add=[("gantt", "Gantt")]
